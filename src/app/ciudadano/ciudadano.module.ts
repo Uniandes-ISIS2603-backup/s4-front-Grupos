7d1003cf481ddee2bdaba9298d0974e731d3e668
Daniel Ramirez <da.ramirez14@uniandes.edu.co>
2018-10-25 01:12:49 -0500

    Fix ciudadano module y comentario module

--- src/app/ciudadano/ciudadano.module.ts
+++ src/app/ciudadano/ciudadano.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { CiudadanoService } from './ciudadano.service';
 import { CiudadanoListComponent } from './ciudadano-list/ciudadano-list.component';
+import {AppRoutingModule} from '../app-routing/app-routing.module';
 
 @NgModule({
     imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
         CommonModule,
+        FormsModule
     ],
     declarations: [CiudadanoListComponent],
     providers: [CiudadanoService],
