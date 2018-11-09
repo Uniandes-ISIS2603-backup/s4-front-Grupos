8442c9ec9c0b01fae158fdf6128a10e6f3cd1003
Josealejandro Barbosa <j.barbosaj@uniandes.edu.co>
2018-11-08 20:18:24 -0500

    cambios en el service

--- src/app/patrocinio/patrocinio.service.ts
+++ src/app/patrocinio/patrocinio.service.ts
@@ -24,11 +24,26 @@ export class PatrocinioService {
    
 
     /**
-     * Returns the Observable object containing the list of ciudadanos retrieved from the API
+     * Returns the Observable object containing the list of patrocinios retrieved from the API
      * @returns The list of ciudadanos in real time
      */
     getPatrocinios(): Observable<Patrocinio[]> {
          return this.http.get<Patrocinio[]>(API_URL +patrocinios);
+    }    
+    /**
+     * Returns the Observable object containing the patrocinio retrieved from the API
+     * @returns The list of ciudadanos in real time
+     */
+     getPatrocinioDetail(patrocinioId): Observable<PatrocinioDetail> {
+        return this.http.get<PatrocinioDetail>(API_URL + patrocinios + '/' + patrocinioId);
+    }
+     /**
+    * Creates a new patrocinio
+    * @param patrocinio The new patrocinio
+    * @returns The patrocinio with its new id if it was created, false if it wasn't
+    */
+   createPatrocinio(patrocinio): Observable<PatrocinioDetail> {
+    return this.http.post<PatrocinioDetail>(API_URL + patrocinios, patrocinio);
     }
 }
 
