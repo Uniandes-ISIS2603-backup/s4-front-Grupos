5b171ab9abf87d2e617f6906055345445ad08fa0
Josealejandro Barbosa <j.barbosaj@uniandes.edu.co>
2018-11-10 20:36:51 -0500

    sol del error de grupos de  interes

--- src/app/grupodeinteres/grupodeinteres.module.ts
+++ src/app/grupodeinteres/grupodeinteres.module.ts
@@ -21,7 +21,7 @@ import { GrupodeinteresCreateComponent } from './grupodeinteres-create/grupodein
     FormsModule,
     NgbModule
   ],
-    declarations: [GrupodeinteresListComponent, GrupodeinteresCreateComponent, GrupodeinteresDetailComponent, GrupodeinteresNoticiaComponent, GrupodeinteresAddNoticiaComponent, GrupodeinteresService],
+    declarations: [GrupodeinteresListComponent, GrupodeinteresCreateComponent, GrupodeinteresDetailComponent, GrupodeinteresNoticiaComponent, GrupodeinteresAddNoticiaComponent],
     providers: [GrupodeinteresService],
     exports: [GrupodeinteresListComponent, GrupodeinteresDetailComponent, GrupodeinteresCreateComponent]
 })
