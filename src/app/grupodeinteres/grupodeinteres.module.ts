f9f2434eee1b7335d7851d65b9877068d7b82413
Daniel Ramirez <da.ramirez14@uniandes.edu.co>
2018-11-15 12:45:49 -0500

    Fixes

--- src/app/grupodeinteres/grupodeinteres.module.ts
+++ src/app/grupodeinteres/grupodeinteres.module.ts
@@ -1,12 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {BrowserModule} from '@angular/platform-browser';
-import {GrupodeinteresAddNoticiaComponent} from './grupodeinteres-add-noticia/grupodeinteres-add-noticia.component';
-import { GrupodeinteresNoticiaComponent } from './grupodeinteres-noticias/grupodeinteres-noticia.component';
 import { GrupodeinteresService } from './grupodeinteres.service';
 import { GrupodeinteresListComponent } from './grupodeinteres-list/grupodeinteres-list.component';
 import { BrowserModule } from '@angular/platform-browser';
@@ -21,9 +14,6 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { GrupodeinteresAddNoticiaComponent } from './grupodeinteres-add-noticia/grupodeinteres-add-noticia.component';
 import { GrupodeinteresAddEventoComponent } from './grupodeinteres-add-evento/grupodeinteres-add-evento.component';
 import { GrupodeinteresCreateComponent } from './grupodeinteres-create/grupodeinteres-create.component';
-
-import { GrupodeinteresDetailComponent } from './grupodeinteres-detail/grupodeinteres-detail.component';
-import { AppRoutingModule } from '../app-routing/app-routing.module';
 @NgModule({
   imports: [
     BrowserModule,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
-/**
- * @ignore
- */
 export const environment = {
   production: true,
-  apiURL: 'http://172.24.41.58:8080/s4_Grupos-api/api'
+  apiURL: 'http://localhost:8080/s4_Grupos-api/api'
 };
